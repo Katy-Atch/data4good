160602822e9b4531ceb240724eb065faf6389fda
Katy Atchison <katherine.atchison20@gmail.com>
2019-12-06 13:00:54 -0600

    Cleaned up jQuery in map.html, added GEO to admin page

--- web/pages/admin.py
+++ web/pages/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from pages.models import Site, CE
+from pages.models import Site, CE, GEO
 
 # Register your models here.
 admin.site.register(Site)
 admin.site.register(CE)
+admin.site.register(GEO)
