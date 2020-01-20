86de7a075c384871d1bdc761ed77ee584bd81a4c
Katy Atchison <katherine.atchison20@gmail.com>
2020-01-19 18:18:08 -0600

    Added the use of Ajax for loading the points that are to be rendered everytime the map zoom changes

--- web/pages/views.py
+++ web/pages/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
-from .models import Site, CE
+from .models import Site, CE, GEO
 from django.urls import reverse
 from django.core.serializers import serialize
 from django.core.management import call_command
+import json
 
 def site(request):
     site_list = list(Site.objects.all())
@@ -12,6 +13,24 @@ def site(request):
 def sponsor(request):
     ce_list = list(CE.objects.all())
     return render(request, "sponsor.html", {'ce_list': ce_list})
+
+def get_site(request):
+    geo_id = request.GET.get('geo_id')
+    site = Site.objects.filter(geo_id=geo_id)[:1]
+    site_to_return = serialize('json', site)
+    return JsonResponse({'site': site_to_return})
+
+def get_sponsor(request):
+    geo_id = request.GET.get('geo_id')
+    sponsor = CE.objects.filter(geo_id=geo_id)[:1]
+    sponsor_to_return = serialize('json', sponsor)
+    return JsonResponse({'sponsor': sponsor_to_return})
+
+def get_geo(request):
+    geo_id = request.GET.get('geo_id')
+    geo = GEO.objects.filter(geo_id=geo_id)[:1]
+    geo_to_return = serialize('json', geo)
+    return JsonResponse({'geo': geo_to_return})
 
 def site_data(request):
     sites = serialize('json', Site.objects.all())
@@ -27,16 +46,28 @@ def sponsor_details(request, ceid):
 
 def points(request):
     if request.method == 'GET':
-        south_west = request.GET['south_west']
-        north_east = request.GET['north_east']
+        data_string = request.GET.get('json_data')
+        data_dict = json.loads(data_string)
+        min_lat = data_dict['min_lat']
+        max_lat = data_dict['max_lat']
+        min_long = data_dict['min_long']
+        max_long = data_dict['max_long']
 
-        site_list = list(Site.objects.all())
-        sites_to_render = list()
-        for site in site_list:
-            if site.latitude >= south_west.latitude and site.latitude <= north_east.latitude \
-                and site.longitude >= south_west.longitude and site.longitude <= north_east.longitude:
-                sites_to_render.append(site)
-
-        return JsonResponse({'result': "success", 'sites': sites_to_render})
+        geo_list = get_geos(min_lat, max_lat, min_long, max_long)
+    
+        geos = serialize('json', geo_list)
+        return JsonResponse({'result': 'success', 'geos': geos})
     else:
-        return JsonResponse({'result': "unsuccessful"})
+        return JsonResponse({'result': 'unsuccessful'})
+
+
+def get_geos(min_lat, max_lat, min_long, max_long):
+    geo_list = GEO.objects.all()
+    geos_to_render = list()
+    for geo in geo_list:
+        if geo.latitude != None and geo.longitude != None:
+            if geo.latitude >= min_lat and geo.latitude <= max_lat \
+                and geo.longitude >= min_long and geo.longitude <= max_long:
+                geos_to_render.append(geo)
+
+    return geos_to_render
